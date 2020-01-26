7446eb7fdf3330399ed2437d875510e5400fd0c3
Filipe Fonseca <filipedfs@gmail.com>
2020-01-26 01:39:19 -0300

    Reformats code to improve performance

--- app/src/main/java/com/filipefonseca/diceroller/MainActivity.kt
+++ app/src/main/java/com/filipefonseca/diceroller/MainActivity.kt
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    // Best practice is to call findViewById as minimum as possible
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initializes the image view, calling findViewById only once
+        diceImage = findViewById(R.id.dice_image)
 
         // Specifies which layout is associate with the activity
         setContentView(R.layout.activity_main)
@@ -18,18 +23,13 @@ class MainActivity : AppCompatActivity() {
         // Get the button
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
-            //            // Make a Toast
-//            Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
             rollDice()
         }
     }
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-//        val resultText: TextView = findViewById(R.id.result_text)
-//        resultText.text = randomInt.toString()
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
